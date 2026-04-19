5d1916d324c7b63b3b90bc788bb4cfe3e25149f5
yuda <yuda9674@gmail.com>
2026-04-19 16:59:06 +0700

    Update keyboardrblx.lua

--- keyboardrblx.lua
+++ keyboardrblx.lua
@@ -65,24 +65,15 @@ bubble.Text = "⌨"
 bubble.BackgroundColor3 = Color3.fromRGB(0,170,255)
 bubble.TextScaled = true
 
--- DRAG FIX (HP + PC)
-local dragging = false
-local dragStart
-local startPos
+-- DRAG SUPER FIX (100% HP + PC)
 
-local function update(input)
-    local delta = input.Position - dragStart
-    bubble.Position = UDim2.new(
-        startPos.X.Scale,
-        startPos.X.Offset + delta.X,
-        startPos.Y.Scale,
-        startPos.Y.Offset + delta.Y
-    )
-end
+local dragging = false
+local dragStart = Vector2.new()
+local startPos = UDim2.new()
 
 bubble.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1
-    or input.UserInputType == Enum.UserInputType.Touch then
+    if input.UserInputType == Enum.UserInputType.Touch 
+    or input.UserInputType == Enum.UserInputType.MouseButton1 then
         
         dragging = true
         dragStart = input.Position
@@ -96,11 +87,19 @@ bubble.InputBegan:Connect(function(input)
     end
 end)
 
-UIS.InputChanged:Connect(function(input)
+bubble.InputChanged:Connect(function(input)
     if dragging and (
-        input.UserInputType == Enum.UserInputType.MouseMovement
-        or input.UserInputType == Enum.UserInputType.Touch) then
-        update(input)
+        input.UserInputType == Enum.UserInputType.Touch 
+        or input.UserInputType == Enum.UserInputType.MouseMovement) then
+        
+        local delta = input.Position - dragStart
+
+        bubble.Position = UDim2.new(
+            startPos.X.Scale,
+            startPos.X.Offset + delta.X,
+            startPos.Y.Scale,
+            startPos.Y.Offset + delta.Y
+        )
     end
 end)
 
